1b5b33684eeecb66357cc786b167067654f15e36
arkadiuszkornecki <arkadiuszkornecki@icloud.com>
2026-07-05 11:25:57 +0200

    fix react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import "@/App.css";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import Bio from "@/routes/bio/Bio.tsx";
 import Quiz from "@/routes/quiz/Quiz.tsx";
 import RavaDocs from "@/routes/rava/RavaDocs.tsx";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Bio />} />
         <Route path="/quiz">
@@ -16,6 +16,6 @@ export default function App() {
 
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
